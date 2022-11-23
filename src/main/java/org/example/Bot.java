d7dd8520618d3a4109a34a652eb6662e308991f5
mixk88 <mixk88@gmail.com>
2022-11-23 22:32:10 +0700

    Add metods for answers to user and sent posts

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -8,6 +8,9 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.IOException;
+
 public class Bot extends TelegramLongPollingBot {
     public static void main(String[] args) throws TelegramApiException {
 
@@ -24,11 +27,11 @@ public class Bot extends TelegramLongPollingBot {
 
         }
     }
-    public void sendMsg(Message message, String text){
+    public void sendPost(String text){
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
-        sendMessage.setChatId(message.getChatId().toString());  //id чата куда отвечать
-        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setChatId("-1001742221166");  //id чата куда отвечать
+        //sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
 
             try{
@@ -37,23 +40,39 @@ public class Bot extends TelegramLongPollingBot {
                 throw new RuntimeException(e);
             }
     }
+    public void sendAnswer(Message message, String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+
+        try{
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
 
         Message message = update.getMessage();
-            if (message !=null && message.hasText()){
-                switch (message.getText()){
-                    case "/help":
-                        sendMsg(message, "can i  help yoy?");
-                        break;
-                    case "/newpost":
-                        sendMsg(message, "new post is");
-                    default:
-                }
+        boolean rightText;
+        try {
+            rightText = Censor.check(message.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (message != null && message.hasText() && rightText == true) {
+            sendPost(message.getText()+ "\n #usersPost");
             }
+        if (message != null && message.hasText() && rightText == false){
+            sendAnswer(message, "Похоже ваше сообщение нарушает правила публикации!" +
+                    "\n Пожалуйста, проверьте текст и попробуйте еще раз!");
+        }
+        }
 
-    }
 
     @Override
     public String getBotUsername() {
@@ -64,4 +83,5 @@ public class Bot extends TelegramLongPollingBot {
     public String getBotToken() {
         return "5864814346:AAEC9rLKdp7HI4lykEJ_hppZeW3e6ViQzlQ";
     }
+
 }
