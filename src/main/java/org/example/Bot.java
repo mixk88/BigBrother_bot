e049d541e9fd389a3f4e11302549aa7babcff088
mixk88 <mixk88@gmail.com>
2022-11-25 00:21:22 +0700

    Add tags adds, fnd posts themes

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -12,6 +12,10 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import java.io.IOException;
 
 public class Bot extends TelegramLongPollingBot {
+    String tags = "";
+    private final static String CHANELID = "-1001742221166";
+    private final static String BOTUSERNAME = "IT_bro_bot";
+    private final static String TOKEN = "5864814346:AAEC9rLKdp7HI4lykEJ_hppZeW3e6ViQzlQ";
     public static void main(String[] args) throws TelegramApiException {
 
 
@@ -30,7 +34,7 @@ public class Bot extends TelegramLongPollingBot {
     public void sendPost(String text){
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
-        sendMessage.setChatId("-1001742221166");  //id чата куда отвечать
+        sendMessage.setChatId(CHANELID);  //id чата куда отвечать
         //sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
 
@@ -56,32 +60,50 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
         Message message = update.getMessage();
+
         boolean rightText;
         try {
             rightText = Censor.check(message.toString());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        if (message != null && message.hasText() && rightText == true) {
-            sendPost(message.getText()+ "\n #usersPost");
-            }
+        if (message != null && message.hasText() && message.getText().equals("/start")){
+            sendAnswer(message, "Привет! Я бот-помошник канала ITeasy!"+
+                    "\n введи одну из комманд в зависимости от предмета публикации");
+
+        }
+        if (message != null && message.hasText() && message.getText().equals("/myProject")){
+           sendAnswer(message, "теперь можешь писать текст своего поста");
+            tags = "#учебныйПроект #некоммерческийПроект #мойПроект";
+        }
+        if (message != null && message.hasText() && message.getText().equals("/vacancy")){
+            sendAnswer(message, "теперь можешь писать текст своего поста");
+            tags = "#вакансия";
+        }
+        if (message != null && message.hasText() && message.getText().equals("/cv")){
+            sendAnswer(message, "теперь можешь писать текст своего поста");
+            tags = "#резюме";
+        }
+
+        if (message != null && message.hasText() && rightText == true && !message.getText().equals("/start")
+         && !message.getText().equals("/vacancy") && !message.getText().equals("/cv") && !message.getText().equals("/myProject")) {
+            sendPost(message.getText() + "\n #userPost "+ tags);
+        }
         if (message != null && message.hasText() && rightText == false){
             sendAnswer(message, "Похоже ваше сообщение нарушает правила публикации!" +
                     "\n Пожалуйста, проверьте текст и попробуйте еще раз!");
         }
-        }
 
+         }
 
     @Override
     public String getBotUsername() {
-        return "IT_bro_bot";
+        return BOTUSERNAME;
     }
 
     @Override
     public String getBotToken() {
-        return "5864814346:AAEC9rLKdp7HI4lykEJ_hppZeW3e6ViQzlQ";
+        return TOKEN;
     }
-
 }
