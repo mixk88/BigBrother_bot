6b7c93a80078a698bc35b81258c6a5167d9e830d
mixk88 <mixk88@gmail.com>
2022-11-28 21:48:11 +0700

    build on spring

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,15 +1,17 @@
 package org.example;
 
 
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
+
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.io.IOException;
+
+
 
 public class Bot extends TelegramLongPollingBot {
     String tags = "";
@@ -17,21 +19,9 @@ public class Bot extends TelegramLongPollingBot {
     public  final static String SERVICEGROUPID = "-712154919";
     private final static String BOTUSERNAME = "IT_bro_bot";
     private final static String TOKEN = "5864814346:AAEC9rLKdp7HI4lykEJ_hppZeW3e6ViQzlQ";
-    public static void main(String[] args) throws TelegramApiException {
 
 
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
 
-        try {
-
-            telegramBotsApi.registerBot(new Bot());
-
-        } catch (TelegramApiException e) {
-
-            e.printStackTrace();
-
-        }
-    }
     public void sendPost(String text){
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
